ed5391de2ee2854cb65db7691cae144d8e8e961d
Richard <richard.carino117@gmail.com>
2020-07-11 11:06:45 +1000

    Done with everything until part 2 number 2

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Please enter a name.")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Please enter a name between 3 and 40 characters.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a location.")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Please enter a location name between 3 and 40 characters.")]
+        public string Location { get; set; }
+    }
+}
