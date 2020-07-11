bac2fdb4eaee6610fdd233792f22021595f89b64
Richard <richard.carino117@gmail.com>
2020-07-11 13:18:54 +1000

    Completed up until Part 2: Controllers #4, however input validation error message is not showing

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -12,24 +15,52 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
-        // GET: /<controller>/
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext jobDbContext)
+        {
+            context = jobDbContext;
+        }
+
+        // GET: /Employer/
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+            return View(employers);
         }
 
         public IActionResult Add()
         {
-            return View();
+            AddEmployerViewModel add = new AddEmployerViewModel();
+            return View(add);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        [HttpPost]
+        public IActionResult Add(AddEmployerViewModel add)
         {
+            if (ModelState.IsValid)
+            {
+                var toAdd = new Employer
+                {
+                    Name = add.Name,
+                    Location = add.Location
+                };
+
+                context.Employers.Add(toAdd);
+                context.SaveChanges();
+            }
             return View();
+            //Tested this form submission, "add" is coming in null and I 
+            //suspect it is because there is no model imported into the view
+            
+            //Also need to check and see why it is not showing the input validation error messages when I submit invalid
+            //input in the form
         }
 
         public IActionResult About(int id)
         {
+
+
             return View();
         }
     }
