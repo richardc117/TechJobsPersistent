18db79f388081f804c719f7cdcd7e007b343de20
Richard <richard.carino117@gmail.com>
2020-07-15 00:16:12 +1000

    SQL done, Ready to turn in

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,18 @@
---Part 1
+-- --Part 1
+SELECT COLUMN_NAME AS "Column Name", DATA_TYPE AS "Data Type"
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_NAME = 'jobs';
 
---Part 2
+-- --Part 2
+SELECT Name AS "Employer Name" FROM employers
+WHERE employers.Location = "St. Louis City, MO";
 
---Part 3
 
+-- --Part 3
+SELECT jobs.Name AS "Job Name", skills.Name AS "Skill Name"
+  FROM jobs
+LEFT OUTER JOIN jobskills
+  ON jobs.Id = jobskills.JobId
+LEFT OUTER JOIN skills
+  ON jobskills.SkillId = skills.Id
+  WHERE jobskills.SkillId IS NOT NULL
